5d674864b1858a96b11ac5f5e0a49167fc6ecf75
Yaroslav <keeperoflife1909@mail.ru>
2022-06-28 21:40:55 +0300

    Задача 0511 до ее решения

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0510/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0510/Solution.java
@@ -11,7 +11,7 @@ public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
 
-        for (int i = 0; i < result.length; i++) {
+        for (int i = 0; i < 10; i++) {
             result[i] = new int[i + 1];
             for (int j = 0; j < result[i].length; j++) {
                 result[i][j] = i + j;
