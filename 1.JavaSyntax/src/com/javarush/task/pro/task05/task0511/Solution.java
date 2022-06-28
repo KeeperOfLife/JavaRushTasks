fffe1b2e519656af191e523316f9acce0f5f4479
Yaroslav <keeperoflife1909@mail.ru>
2022-06-28 23:22:47 +0300

    Задача 0511 после ее решения

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0511/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0511/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.pro.task05.task0511;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /* 
@@ -11,5 +12,22 @@ public class Solution {
 
     public static void main(String[] args) {
         //напишите тут ваш код
+        Scanner console = new Scanner(System.in);
+        int N = console.nextInt();
+        multiArray = new int[N][];
+        for (int i = 0; i < N; i++) {
+            int a = console.nextInt();
+            multiArray[i] = new int[a];
+        }
+        System.out.println(Arrays.deepToString(multiArray));
     }
 }
+
+
+
+
+
+
+
+
+
