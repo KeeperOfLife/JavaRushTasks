d04a72fadd38a233c7dc471561c0b4b7a09c56df
Yaroslav <keeperoflife1909@mail.ru>
2022-06-29 14:42:26 +0300

    Задача 0627 до ее решения

---
+++ 1.JavaSyntax/src/com/javarush/task/jdk13/task06/task0627/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.jdk13.task06.task0627;
+
+import java.util.Scanner;
+
+/* 
+Вот это переворот!
+*/
+
+public class Solution {
+    public static int[][] array = new int[3][3];
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
