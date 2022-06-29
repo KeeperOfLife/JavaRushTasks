163bdb85022e398d0eb481261f069689276e5c31
Yaroslav <keeperoflife1909@mail.ru>
2022-06-29 14:34:37 +0300

    Задача 0626 после ее решения

--- 1.JavaSyntax/src/com/javarush/task/jdk13/task06/task0626/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/jdk13/task06/task0626/Solution.java
@@ -8,14 +8,22 @@ import java.util.Scanner;
 
 public class Solution {
     public static String[][] array = new String[][]{{"123", "Иванов", "Богдан"},
-                                                    {"1425", "Петрова", "Марина"},
-                                                    {"37", "Богдан", "Андрей"},
-                                                    {"98", "Богданова", "Марина"},
-                                                    {"6285", "Прутко", "Сергей"},
-                                                    {"8", "Клочкова", "Елена"},
-                                                    {"754", "Котов", "Иван"}};
+            {"1425", "Петрова", "Марина"},
+            {"37", "Богдан", "Андрей"},
+            {"98", "Богданова", "Марина"},
+            {"6285", "Прутко", "Сергей"},
+            {"8", "Клочкова", "Елена"},
+            {"754", "Котов", "Иван"}};
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+
+        Scanner console = new Scanner(System.in);
+        String id = console.nextLine();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i][0].equals(id) || array[i][1].equals(id) || array[i][2].equals(id)){
+                System.out.print(array[i][0] + " " + array [i][1] + " " + array[i][2]);
+                System.out.println();
+            }
+        }
     }
 }
