f808368fe5a21e0052d75a8b4989c9d3f4fdff1a
Yaroslav <keeperoflife1909@mail.ru>
2022-06-29 15:27:14 +0300

    Задача 0627 после ее решения

--- 1.JavaSyntax/src/com/javarush/task/jdk13/task06/task0627/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/jdk13/task06/task0627/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.jdk13.task06.task0627;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /* 
@@ -11,5 +12,14 @@ public class Solution {
 
     public static void main(String[] args) {
         //напишите тут ваш код
+        Scanner console = new Scanner(System.in);
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[j][i] = console.nextInt();
+            }
+        }
+        System.out.println(array[0][0] + " " + array [0][1] + " " + array[0][2]);
+        System.out.println(array[1][0] + " " + array [1][1] + " " + array[1][2]);
+        System.out.println(array[2][0] + " " + array [2][1] + " " + array[2][2]);
     }
 }
