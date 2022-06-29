775fd9ab0cf9c22b986f48c5f7f555033cf2c9c7
Yaroslav <keeperoflife1909@mail.ru>
2022-06-29 13:30:56 +0300

    Задача 0626 до ее решения

---
+++ 1.JavaSyntax/src/com/javarush/task/jdk13/task06/task0626/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.jdk13.task06.task0626;
+
+import java.util.Scanner;
+
+/* 
+Первая база данных
+*/
+
+public class Solution {
+    public static String[][] array = new String[][]{{"123", "Иванов", "Богдан"},
+                                                    {"1425", "Петрова", "Марина"},
+                                                    {"37", "Богдан", "Андрей"},
+                                                    {"98", "Богданова", "Марина"},
+                                                    {"6285", "Прутко", "Сергей"},
+                                                    {"8", "Клочкова", "Елена"},
+                                                    {"754", "Котов", "Иван"}};
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
