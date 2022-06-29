3a841afaf0286948150be56c2cf8b38d61fe34e4
Yaroslav <keeperoflife1909@mail.ru>
2022-06-29 18:14:41 +0300

    Задача 0627 после ее решения по условию

--- 1.JavaSyntax/src/com/javarush/task/jdk13/task06/task0627/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/jdk13/task06/task0627/Solution.java
@@ -1,6 +1,5 @@
 package com.javarush.task.jdk13.task06.task0627;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 /* 
@@ -11,15 +10,27 @@ public class Solution {
     public static int[][] array = new int[3][3];
 
     public static void main(String[] args) {
-        //напишите тут ваш код
         Scanner console = new Scanner(System.in);
+
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                array[j][i] = console.nextInt();
+            for (int j = 0; j < array[0].length; j++) {
+                array[i][j] = console.nextInt();
             }
         }
-        System.out.println(array[0][0] + " " + array [0][1] + " " + array[0][2]);
-        System.out.println(array[1][0] + " " + array [1][1] + " " + array[1][2]);
-        System.out.println(array[2][0] + " " + array [2][1] + " " + array[2][2]);
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array[0].length; j++) {
+                int temp = array[i][j];
+                array[i][j] = array[j][i];
+                array[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
