f4cf89d56d763c5bf02eadd31e444b36c83d86ef
Yaroslav <keeperoflife1909@mail.ru>
2022-07-08 16:35:26 +0300

    Задача 0906 после ее решения по условию

--- 1.JavaSyntax/src/com/javarush/task/pro/task09/task0906/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0906/Solution.java
@@ -1,8 +1,6 @@
 package com.javarush.task.pro.task09.task0906;
 
-import java.util.regex.Pattern;
-
-/* 
+/*
 Двоичный конвертер
 */
 
@@ -16,11 +14,30 @@ public class Solution {
 
     public static String toBinary(int decimalNumber) {
         //напишите тут ваш код
-        return null;
+        String toBinary = "";
+        // Если входящий параметр меньше или равен 0, метод toBinary(int) возвращает пустую строку
+        if (decimalNumber <= 0) {
+            return toBinary;
+        }
+        while (decimalNumber != 0) {
+            toBinary = decimalNumber % 2 + toBinary;
+            decimalNumber /= 2;
+        }
+        return toBinary;
     }
 
     public static int toDecimal(String binaryNumber) {
         //напишите тут ваш код
-        return 0;
+        int toDecimal = 0;
+        if (binaryNumber == null) {
+            return toDecimal;
+        }
+
+        for (int i = 0; i < binaryNumber.length(); i++) {
+            int index = binaryNumber.length() - 1 - i;
+            int value = Character.getNumericValue(binaryNumber.charAt(index));
+            toDecimal += value * Math.pow(2, i);
+        }
+        return toDecimal;
     }
 }
